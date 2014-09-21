08160404bfbe3bf0eaf93358785eff807434c087
jamisliao <jamisliao@accuvally.com>
2014-09-21 11:25:10 +0800

    5

--- JenkinsTest_ExampleTests/Models/CalculatorTests.cs
+++ JenkinsTest_ExampleTests/Models/CalculatorTests.cs
@@ -15,7 +15,7 @@ namespace Models.Tests
         {
             //Test
             int i = 0;
-            i = i + 4;
+            i = i + 5;
             var expect = 5;
             var target = new Calculator();
             var actual = target.Add(2, 3);
